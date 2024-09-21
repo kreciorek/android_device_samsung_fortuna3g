6cec6310c7bd92ee42a7b58c6f0ca42707927519
Ksawlii <ksawlii.do.plikow@gmail.com>
2024-09-21 18:43:56 +0000

    fortuna3g: Fixed recovery image size

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,7 @@ LOCAL_PATH := device/samsung/fortuna3g
 TARGET_OTA_ASSERT_DEVICE := fortuna3g,fortuna3gxx
 
 # Fix recovery image size
-BOARD_RECOVERYIMAGE_PARTITION_SIZE  := 15972368
+BOARD_RECOVERYIMAGE_PARTITION_SIZE  := 15728640
 
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortuna3g_eur_defconfig
